8185ff68dd61cb9b65e024971f22e52c638077fa
Ikrom <74912914+Ikrom27@users.noreply.github.com>
2024-12-13 00:30:27 +0300

    remote server added

--- lib/data/Models.dart
+++ lib/data/Models.dart
@@ -13,6 +13,25 @@ class ProductModel {
     required this.imageUrl
   });
 
+  factory ProductModel.fromJson(Map<String, dynamic> json){
+    return switch (json) {
+      {
+      'id': int id,
+      'name': String name,
+      'description': String description,
+      'price': int price,
+      'image_url': String imageUrl,
+    } => ProductModel(
+        id: id,
+        name: name,
+        price: price.toDouble(),
+        description: description,
+        imageUrl: imageUrl
+      ),
+    _ => throw const FormatException("Product convert fail")
+    };
+  }
+
   @override
   String toString() {
     return 'Product(name: $name, price: $price, description: $description)';
--- lib/data/RemoteDataSource.dart
+++ lib/data/RemoteDataSource.dart
@@ -1,52 +1,20 @@
-import 'package:coffee_house/data/Models.dart';
+import 'dart:convert';
 
-List<ProductModel> MenuTestData = [
-  ProductModel(
-      id: 1,
-      name: "Americano",
-      price: 90,
-      description: "bestCoffe",
-      imageUrl: "https://avatars.mds.yandex.net/get-altay/11381866/2a0000018c58b41338dee13b0dbe5c852462/XXL_height"
-  ),
-  ProductModel(
-      id: 2,
-      name: "Americano",
-      price: 90,
-      description: "bestCoffe",
-      imageUrl: "https://puzzleit.ru/files/puzzles/153/152863/_original.jpg"
-  ),
-  ProductModel(
-      id: 3,
-      name: "Americano",
-      price: 90,
-      description: "bestCoffe",
-      imageUrl: "https://avatars.mds.yandex.net/i?id=bf46d7626d890b0fac4aa23773887f4f_l-5647863-images-thumbs&n=13"
-  ),
-  ProductModel(
-      id: 4,
-      name: "Americano",
-      price: 90,
-      description: "bestCoffe",
-      imageUrl: "https://avatars.mds.yandex.net/i?id=3cb280cbc31d2937d7574161ea40f228_l-4565320-images-thumbs&n=13"
-  ),
-  ProductModel(
-      id: 4,
-      name: "Americano",
-      price: 90,
-      description: "bestCoffe",
-      imageUrl: "https://avatars.mds.yandex.net/i?id=2b9620c174c1749f1402b9d8037d0089_l-5356399-images-thumbs&n=13"
-  ),
-  ProductModel(
-      id: 4,
-      name: "Americano",
-      price: 90,
-      description: "bestCoffe",
-      imageUrl: "https://raivbel.by/upload/iblock/992/992f824b818d5741bbb00b9ff54c415b.jpg"
-  ),
-];
+import 'package:coffee_house/data/Models.dart';
+import 'package:http/http.dart' as http;
+
+String baseUrl = "https://bbaupg6vmhmlgvvc816r.containers.yandexcloud.net";
 
 class RemoteDataSource {
-  Future<List<ProductModel>> fetchProducts() {
-    return Future.delayed(const Duration(seconds: 3), () => MenuTestData);
+  Future<List<ProductModel>> fetchProducts() async {
+    final response = await http.get(Uri.parse("$baseUrl/coffees"));
+    if (response.statusCode == 200) {
+      List<dynamic> jsonList = json.decode(response.body);
+      return jsonList
+          .map((jsonItem) => ProductModel.fromJson(jsonItem))
+          .toList();
+    } else {
+      throw Exception('Failed to load products');
+    }
   }
 }
